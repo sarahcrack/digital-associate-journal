8e07f1888590394db7be894f73e4ad61e0513a61
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-09-17 16:38:23 +0100

    schema syntax updated by Rubocop and added check for post date to determine if WEEK will be displayed

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_29_160849) do
+ActiveRecord::Schema[7.1].define(version: 20_240_829_160_849) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "posts", force: :cascade do |t|
-    t.integer "week"
-    t.string "digital_discipline"
-    t.date "date"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'posts', force: :cascade do |t|
+    t.integer 'week'
+    t.string 'digital_discipline'
+    t.date 'date'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
