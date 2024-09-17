7286598ad8c795e0ce3fc97c94d8d409b52c7246
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2024-09-17 15:30:38 +0100

    added comments to helper method file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+# helper methods that can be used in views
 module ApplicationHelper
-  def ordinalize_day(date) # Get the day part from the date (e.g., 5 from March 5th)
+  # Get the day part from the date (e.g., 5 from March 5th)
+  def ordinalize_day(date)
     day = date.day
-      # Determine the ordinal suffix ('st', 'nd', 'rd', 'th') for the day
-    ordinal = case day % 10 # This checks the last digit of the day (using modulus) to determine the ordinal suffix. 
+    # Determine the ordinal suffix ('st', 'nd', 'rd', 'th') for the day
+    ordinal = case day % 10 # This checks the last digit of the day (using modulus) to determine the ordinal suffix.
               when 1
                 (day == 11 ? 'th' : 'st') # 11th is special case, otherwise it's 'st'
               when 2
-                (day == 12 ? 'th' : 'nd') # 12th is special case, otherwise it's 'nd' 
+                (day == 12 ? 'th' : 'nd') # 12th is special case, otherwise it's 'nd'
               when 3
                 (day == 13 ? 'th' : 'rd') # 13th is special case, otherwise it's 'rd'
               else
